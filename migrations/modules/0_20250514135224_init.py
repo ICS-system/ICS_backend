e4ff142f9fe3127b380611548d40ff66ab700474
jiwon <realjiwon98@naver.com>
2025-05-16 11:03:00 +0900

    사용자 비즈니스 로직 완료

---
+++ migrations/modules/0_20250514135224_init.py
@@ -0,0 +1,26 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS `aerich` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `version` VARCHAR(255) NOT NULL,
+    `app` VARCHAR(100) NOT NULL,
+    `content` JSON NOT NULL
+) CHARACTER SET utf8mb4;
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `modified_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `username` VARCHAR(40) NOT NULL UNIQUE,
+    `password` VARCHAR(128) NOT NULL,
+    `full_name` VARCHAR(40) NOT NULL,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `agree_terms` BOOL NOT NULL DEFAULT 0
+) CHARACTER SET utf8mb4;"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """

23e637937ac674528a515901e894ed5eb2010489
jiwon <realjiwon98@naver.com>
2025-05-29 13:24:41 +0900

    1. CORS 메서드 옵션 와일드 카드로 변경 2. live 모델 추가 3. live 라우터 추가

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from starlette.responses import JSONResponse
 from tortoise.contrib.fastapi import register_tortoise
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -11,12 +13,26 @@ DB_URL = os.getenv("DB_URL")
 
 app = FastAPI()
 
+# 로그인 이후 프로필 정보 변경 시 나타나는 422 오류 로그 확인
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
+    print(f"422 오류 발생: {exc_str}")
+    content = {
+        "status_code": 10422,
+        "message": exc_str,
+        "data": None,
+    }
+    return JSONResponse(
+        content,
+        status_code = status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 # CORS 미들웨어 관리
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUSH"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
@@ -24,12 +40,14 @@ app.add_middleware(
 register_tortoise(
     app,
     db_url=DB_URL,
-    modules={"models": ["app.models.user_model"]},
+    modules={"models": ["app.models.user_model", "app.models.live_model"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
 
 # 라우터 등록 관리
 from app.routers.user_router import router as user_router
+from app.routers.live_router import router as live_router
 
 app.include_router(user_router)
+app.include_router(live_router)

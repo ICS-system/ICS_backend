ae4e04bc82753d317e914f821f52aa0a9e5b82e7
jiwon <realjiwon98@naver.com>
2025-05-29 13:22:55 +0900

    janus_service 등록 및 연결

---
+++ app/services/janus_service.py
@@ -0,0 +1,6 @@
+async def create_videoroom(self, room_id: int, description: str) -> dict[int, str]:
+    """Videoroom 생성 (Admin API 사용)"""
+
+
+async def destroy_videoroom(self, room_id: int, description: str) -> dict[int, str]:
+    """VideoRoom 삭제 (Admin API 사용) """

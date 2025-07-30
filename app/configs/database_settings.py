157b1b8f0a31f852b60dae4e58733a3a8bdb99ea
jiwon <realjiwon98@naver.com>
2025-07-30 15:51:06 +0900

    DB_URL 및 TORTOISE_APP_MODELS 추가

--- app/configs/database_settings.py
+++ app/configs/database_settings.py
@@ -1,8 +1,15 @@
+import os
 from app.configs import settings
+
+DB_URL = (
+    f"mysql://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_DB}"
+)
 
 TORTOISE_APP_MODELS = [
     "aerich.models",
     "app.models.user_model",
+    "app.models.live_model",
 ]
 
 TORTOISE_ORM = {
@@ -23,8 +30,9 @@ TORTOISE_ORM = {
         },
     },
     "apps": {
-        "modules": {
+        "models": {
             "models": TORTOISE_APP_MODELS,
+            "default_connection": "default",
         },
     },
     # "routers": ["app.configs.database_config.Router"],
